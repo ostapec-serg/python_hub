ab93ff52649e6084fca1b680f85ef588532e2c82
Sergii Ostapec <ostapec.serg@gmail.com>
2021-12-02 18:09:39 +0200

    Changed range function. Task 6

--- HT_5/HT5_task_6.py
+++ HT_5/HT5_task_6.py
@@ -3,27 +3,30 @@
 #  P.S. Generator must return.
 
 def value_func(start: int,stop=0,step=1):
-    if start > stop and step > 0:
+    if stop+1 == start:
         yield []
     else:
-        if step > 0:
-            if stop == 0:
-                stop = start
-                start = 0
-            while start < stop:
-                yield start
-                start += step
-        elif (step < 0) and (stop < 0):
-            if stop == 0:
-                stop = start
-                start = 0
-            while start > stop:
-                yield start
-                start += step
-        elif step == 0:
-            raise ValueError("step cannot be 0")
-        else:
+        if start == stop:
             yield []
+        else:
+            if step > 0:
+                if stop == 0:
+                    stop = start
+                    start = 0
+                while start < stop:
+                    yield start
+                    start += step
+            elif (step < 0) and (stop < 0):
+                if stop == 0:
+                    stop = start
+                    start = 0
+                while start > stop:
+                    yield start
+                    start += step
+            elif step == 0:
+                raise ValueError("step cannot be 0")
+            else:
+                yield []
 
-for elem in value_func(0, 10, 1):
+for elem in value_func(10):
     print(elem)
